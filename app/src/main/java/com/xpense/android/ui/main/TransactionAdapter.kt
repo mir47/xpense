4256870ed1f735222de111110f79dadcde55cd09
miro <minev.miro@gmail.com>
2022-01-30 15:12:57 +0200

    Refactor adapter

--- app/src/main/java/com/xpense/android/ui/main/TransactionAdapter.kt
+++ app/src/main/java/com/xpense/android/ui/main/TransactionAdapter.kt
@@ -8,47 +8,36 @@ import androidx.recyclerview.widget.RecyclerView
 import com.xpense.android.databinding.ItemTransactionBinding
 import com.xpense.android.db.Transaction
 
-class TransactionAdapter : ListAdapter<Transaction, RecyclerView.ViewHolder>(TransactionDiffCallback()) {
+class TransactionAdapter :
+    ListAdapter<Transaction, TransactionAdapter.TransactionViewHolder>(TransactionDiffCallback()) {
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val item = getItem(position) as Transaction
-        (holder as TransactionViewHolder).bind(item)
-    }
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) =
+        holder.bind(getItem(position))
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return TransactionViewHolder.from(parent)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        TransactionViewHolder(ItemTransactionBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false))
 
-    class TransactionViewHolder private constructor(private val binding: ItemTransactionBinding)
+    class TransactionViewHolder(private val binding: ItemTransactionBinding)
         : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Transaction) {
             binding.transaction = item
-            binding.executePendingBindings()
-        }
-
-        companion object {
-            fun from(parent: ViewGroup): TransactionViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemTransactionBinding.inflate(layoutInflater, parent, false)
-                return TransactionViewHolder(binding)
-            }
+//            binding.executePendingBindings()
         }
     }
-}
 
-/**
- * Callback for calculating the diff between two non-null items in a list.
- *
- * Used by ListAdapter to calculate the minimum number of changes between
- * and old list and a new list that's been passed to `submitList`.
- */
-class TransactionDiffCallback : DiffUtil.ItemCallback<Transaction>() {
-    override fun areItemsTheSame(oldItem: Transaction, newItem: Transaction): Boolean {
-        return oldItem.transactionId == newItem.transactionId
-    }
+    /**
+     * Callback for calculating the diff between two non-null items in a list.
+     *
+     * Used by ListAdapter to calculate the minimum number of changes between
+     * and old list and a new list that's been passed to `submitList`.
+     */
+    class TransactionDiffCallback : DiffUtil.ItemCallback<Transaction>() {
+        override fun areItemsTheSame(oldItem: Transaction, newItem: Transaction) =
+            oldItem.transactionId == newItem.transactionId
 
-    override fun areContentsTheSame(oldItem: Transaction, newItem: Transaction): Boolean {
-        return oldItem == newItem
+        override fun areContentsTheSame(oldItem: Transaction, newItem: Transaction) =
+            oldItem == newItem
     }
 }
