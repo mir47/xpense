a1aae0268cb294e7d1bfd7c4b46849c7a1af3313
miro <minev.miro@gmail.com>
2022-02-06 22:31:43 +0200

    Comment out sms receiver and bump version

--- app/src/main/java/com/xpense/android/receiver/SmsReceiver.kt
+++ app/src/main/java/com/xpense/android/receiver/SmsReceiver.kt
@@ -1,39 +1,40 @@
 package com.xpense.android.receiver
 
-import android.app.NotificationManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import android.telephony.SmsMessage
-import com.xpense.android.util.sendNotification
+//import android.app.NotificationManager
+//import android.content.BroadcastReceiver
+//import android.content.Context
+//import android.content.Intent
+//import android.os.Bundle
+//import android.telephony.SmsMessage
+//import com.xpense.android.util.sendNotification
 
-class SmsReceiver : BroadcastReceiver() {
-
-    override fun onReceive(context: Context, intent: Intent) {
-        println("mmmmm Intent received: " + intent.action)
-
-        if (intent.action === SMS_RECEIVED) {
-            val bundle: Bundle? = intent.extras
-            if (bundle != null) {
-                val pdus = bundle["pdus"] as Array<*>?
-                val messages: Array<SmsMessage?> = arrayOfNulls(pdus!!.size)
-                for (i in pdus.indices) {
-                    messages[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray)
-                }
-                if (messages.size > -1) {
-                    println("mmmmm Message received: " + messages[0]?.messageBody)
-                    println("mmmmm Message received from: " + messages[0]?.originatingAddress)
-
-                    val notificationManager: NotificationManager =
-                        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                    notificationManager.sendNotification(context)
-                }
-            }
-        }
-    }
-
-    companion object {
-        const val SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED"
-    }
-}
+// TODO: re-enable when SMS receiver permission is resolved on Play Store
+//class SmsReceiver : BroadcastReceiver() {
+//
+//    override fun onReceive(context: Context, intent: Intent) {
+//        println("mmmmm Intent received: " + intent.action)
+//
+//        if (intent.action === SMS_RECEIVED) {
+//            val bundle: Bundle? = intent.extras
+//            if (bundle != null) {
+//                val pdus = bundle["pdus"] as Array<*>?
+//                val messages: Array<SmsMessage?> = arrayOfNulls(pdus!!.size)
+//                for (i in pdus.indices) {
+//                    messages[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray)
+//                }
+//                if (messages.size > -1) {
+//                    println("mmmmm Message received: " + messages[0]?.messageBody)
+//                    println("mmmmm Message received from: " + messages[0]?.originatingAddress)
+//
+//                    val notificationManager: NotificationManager =
+//                        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//                    notificationManager.sendNotification(context)
+//                }
+//            }
+//        }
+//    }
+//
+//    companion object {
+//        const val SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED"
+//    }
+//}
