4fa38cf4313d49111cd9d55134c525597d8e4ea0
miro <minev.miro@gmail.com>
2022-02-26 14:48:47 +0200

    Implement login using Firebase Auth and Firebase UI

---
+++ app/src/main/java/com/xpense/android/ui/experiments/login/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.xpense.android.ui.experiments.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+
+class LoginViewModel : ViewModel() {
+
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+    }
+
+    val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
+}
