85b5cbed2e952dae399405b97c9af8fe5d223c18
miro <minev.miro@gmail.com>
2022-10-04 00:01:37 +0200

    UI improvements

--- app/src/main/java/com/xpense/android/ui/theme/Theme.kt
+++ app/src/main/java/com/xpense/android/ui/theme/Theme.kt
@@ -1,42 +1,36 @@
-package com.xpense.android.presentation.ui.theme
+package com.xpense.android.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-//    primary = Purple200,
+    primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200,
+    background = Color.Black,
+    surface = Color.Black,
 
-    // from Compose Basics Codelab:
-    surface = Blue,
-    onSurface = Navy,
-    primary = Navy,
-    onPrimary = Chartreuse
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color.White,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200,
-
-    // from Compose Basics Codelab:
-//    surface = Blue,
-//    onSurface = Color.White,
-//    primary = LightBlue,
-//    onPrimary = Navy
-
-    /* Other default colors to override
     background = Color.White,
     surface = Color.White,
+
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
--- app/src/main/java/com/xpense/android/ui/txn/list/TxnListViewModel.kt
+++ app/src/main/java/com/xpense/android/ui/txn/list/TxnListViewModel.kt
@@ -5,15 +5,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.xpense.android.data.Result
+import com.xpense.android.domain.use_case.DeleteTxnsUseCase
 import com.xpense.android.domain.use_case.ObserveTxnsResultUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TxnListViewModel @Inject constructor(
     private val observeTxnsResultUseCase: ObserveTxnsResultUseCase,
+    private val deleteTxnsUseCase: DeleteTxnsUseCase,
 ) : ViewModel() {
 
     private val _state = mutableStateOf<UiState>(UiState.Loading)
@@ -21,6 +25,12 @@ class TxnListViewModel @Inject constructor(
 
     init {
         observeTxns()
+    }
+
+    fun deleteAllTxns() {
+        viewModelScope.launch(Dispatchers.IO) {
+            deleteTxnsUseCase()
+        }
     }
 
     private fun observeTxns() {
