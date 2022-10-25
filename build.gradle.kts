dd8c741feb715e49bfdfbab78bc3e4baa48b0834
miro <minev.miro@gmail.com>
2022-10-25 23:46:32 +0200

    add plugin to check for updates to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    // plugin to check for updates to dependencies and Gradle itself.
+    // source: https://github.com/ben-manes/gradle-versions-plugin
+    // run: ./gradlew dependencyUpdates
+    id("com.github.ben-manes.versions") version "0.28.0"
+}
+
 buildscript {
     repositories {
         google()
