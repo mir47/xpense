0b349d6557bd78c39ee803ce188788f0eca381c3
miro <minev.miro@gmail.com>
2022-10-25 22:48:47 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         targetSdk = Apps.targetSdk
         // todo: move version code and name to Dependencies.Apps (buildSrc module)
         //  when fastlane increment_version_code and increment_version_name supports it
-        versionCode = 57
-        versionName = "0.55.0"
+        versionCode = 58
+        versionName = "0.56.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val key: String = gradleLocalProperties(rootDir).getProperty("MAPS_API_KEY")
         buildConfigField ("String", "MAPS_API_KEY", key)
