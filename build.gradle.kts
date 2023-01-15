86ca649c2584b947ce25656b8f22fffd1eb760f3
miro <minev.miro@gmail.com>
2023-01-15 02:33:36 +0200

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     // plugin to check for updates to dependencies and Gradle itself.
     // source: https://github.com/ben-manes/gradle-versions-plugin
     // run: ./gradlew dependencyUpdates
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
 }
 
 buildscript {
